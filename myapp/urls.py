f8bbe57edc247806f9bc73f059e9cf4b804245d5
RangaKomalSai <ranga.komal.sai@gmail.com>
2024-07-25 19:00:43 +0530

    urls changed

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/google-login/', views.google_login, name='google_login'),
-    path('api/districts/<str:state_name>/', views.GetDistrictsView.as_view(), name='get_districts'),
-    path('api/colleges/<str:state_name>/<str:district_name>/', views.GetCollegesView.as_view(), name='get_colleges'),
-    path('api/submit-form/', views.SubmitFormView.as_view(), name='submit_form'),
+    path('myapi/google-login/', views.google_login, name='google_login'),
+    path('myapi/districts/<str:state_name>/', views.GetDistrictsView.as_view(), name='get_districts'),
+    path('myapi/colleges/<str:state_name>/<str:district_name>/', views.GetCollegesView.as_view(), name='get_colleges'),
+    path('myapi/submit-form/', views.SubmitFormView.as_view(), name='submit_form'),
 ]
